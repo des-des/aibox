e5c5d5e58d6b9da1b7f0244abbb3fcc866c96189
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-18 12:36:36 +0100

    basic form on front end which sends username and password as a put request

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+var signupForm = document.querySelector('form.authenticate');
+var log = console.log;
+
+// console.log(signupForm);
+signupForm.addEventListener('submit', signupSubmit);
+
+function signupSubmit(event) {
+  var form = event.srcElement;
+  var username = getFirstElementByNameFrom(form, 'username').value;
+  var password = getFirstElementByNameFrom(form, 'password').value;
+
+  event.preventDefault();
+
+  sendCreateUserRequest(username, password, function() {
+    console.log('account creation success');
+  });
+}
+
+function sendCreateUserRequest(username, password, callback) {
+  var userData = JSON.stringify({
+    username: username,
+    password: password
+  });
+  var postNewUserRequest = new XMLHttpRequest();
+  postNewUserRequest.open('POST', '../data/newuser');
+  postNewUserRequest.onreadystatechange = function() {
+    postNewUserResponseHandler(postNewUserRequest, callback)
+  };
+  postNewUserRequest.send(userData);
+}
+
+function postNewUserResponseHandler(postNewUserRequest, callback) {
+  var responseReadyState = postNewUserRequest.readyState
+    , responseStatusCode
+    , responseText;
+  if (responseReadyState === 4) {
+    responseStatusCode = postNewUserRequest.status;
+    if (responseStatusCode === 200) {
+      callback();
+    } else {
+      logRequestError(postNewUserRequest, 'New user');
+    }
+  }
+}
+
+function logRequestError(request, requestDescription) {
+  console.log(requestDescription + ' request failed with status code ' +
+    request.status);
+}
+
+function getFirstElementByNameFrom(elementCollection, nameToFind) {
+  var elementArray = arrayFrom(elementCollection);
+  var matches = [];
+  elementArray.forEach(function(element) {
+    if (element.name === nameToFind) {
+      matches.push(element);
+    }
+  });
+  if (matches.length === 1) {
+    return matches[0];
+  }
+}
+
+function arrayFrom(enumerable) {
+  return [].slice.apply(enumerable);
+}
