6e0177f81840eff397aa8d66f84eebc9f373d2c2
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-31 16:36:35 +0000

    draw flow chart from adjacency matrix

--- public/js/mat.js
+++ public/js/mat.js
@@ -1,153 +1,18 @@
-// [
-//  [0, 1],
-//  [0, 0]
-// ]
-//
-// o
-// |
-// o
-//
-// [
-//  [0, 1, 0],
-//  [0, 0, 1],
-//  [0, 0, 0]
-// ]
-//
-// o
-// |
-// o
-// |
-// o
-//
-// [
-//  [0, 1, 1, 0],//1
-//  [0, 0, 0, 1],//2
-//  [0, 0, 0, 1],//3
-//  [0, 0, 0, 0] //4
-// ]
-//
-//  o1
-//  ^
-// o2 o3
-//  v
-//  o4
-//
-//  [
-//   [0, 1, 0, 0],//1
-//   [0, 0, 1, 0],//2
-//   [0, 0, 0, 1],//3
-//   [0, 0, 0, 0] //4
-//  ]
-//
-//  o1
-//  |
-//  o2
-//  |
-//  o3
-//  |
-//  o4
-//
-//  [//1, 2, 3, 4, 5, 6
-//    [0, 1, 1, 0, 0, 0],//1
-//    [0, 0, 0, 1, 0, 0],//2
-//    [0, 0, 0, 0, 1, 0],//3
-//    [0, 0, 0, 0, 0, 1] //4
-//    [0, 0, 0, 0, 0, 1] //4
-//    [0, 0, 0, 0, 0, 0] //4
-//  ]
-//
-//   o1
-//   ^
-// o2 o3
-// |  |
-// o4 o5
-//   V
-//   o6
-//
-//
-//  [//1, 2, 3, 4, 5
-//    [0, 1, 1, 0, 0],//1
-//    [0, 0, 0, 1, 0],//2
-//    [0, 0, 0, 0, 1],//3
-//    [0, 0, 0, 0, 1] //4
-//    [0, 0, 0, 0, 0] //4
-//  ]
-//
-//   o1
-//   ^
-// o2 o3
-// |  |
-// o4 |
-//   V
-//   o5
-//
-//
-//   [//1, 2, 3, 4, 5
-//    [0, 1, 1, 0, 0],//1
-//    [0, 0, 0, 0, 1],//2
-//    [0, 0, 0, 1, 0],//3
-//    [0, 0, 0, 0, 1] //4
-//    [0, 0, 0, 0, 0] //5
-//   ]
-//
-//   o1
-//   ^
-// o2 o3
-// |  |
-// | o4
-//  V
-//  o5
-//
-//  start a 1
-//  1) y = 0, x = 0;
-//  1 -> 2, 3 (2, 3)
-//  2) y = 1, x = 0 (3, 2->5)
-//  3) y = 1, x = 1 (3->4, 5)
-//  4) y = 2, x = 1 (4->5, 5) merge -> choose bigger y, smaller x
-//  5) y = 3, x = 0 END
-//
-//  [//1, 2, 3, 4, 5
-//   [0, 1, 1, 0, 0],//1
-//   [0, 0, 0, 0, 1],//2
-//   [0, 0, 0, 1, 0],//3
-//   [0, 0, 0, 0, 1] //4
-//   [0, 0, 0, 0, 0] //5
-//  ]
-//
-//   o1
-//    ^
-// o2  o3
-// |   ^
-// | o4 o5
-// |  | |
-// | o6 |
-//  V   |
-//  o7  |
-//    V
-//    o8
-//
-// [//1, 2, 3, 4, 5, 6, 7, 8
-//   [0, 1, 1, 0, 0, 0, 0, 0],  //1
-//   [0, 0, 0, 0, 0, 0, 1, 0],  //2
-//   [0, 0, 0, 1, 1, 0, 0, 0],  //3
-//   [0, 0, 0, 0, 0, 1, 0, 0],  //4
-//   [0, 0, 0, 0, 0, 0, 0, 1],  //5
-//   [0, 0, 0, 0, 0, 0, 1, 0],  //6
-//   [0, 0, 0, 0, 0, 0, 0, 1],  //7
-//   [0, 0, 0, 0, 0, 0, 0, 0],  //8
-// ]
-//
-// a) set current to (1) = {x:0, y:0}
-// b) create link nodes
-// c) push link nodes into next
-// d) sort link nodes
-// e) resolve duplicates
-// f) set current to first pop next
-// g) if current return to b)
-
 var getNodes = function(mat) {
-  return getNextNodes(mat, createNode(0, 0, 0), []);
+  var makeId = createIdMaker(mat.length);
+  var result = getNextNodes(mat, createNode(0, '', 0), []);
+  var spaces = fillSpaces(result.nodes, result.links, makeId);
+  result = {
+    nodes: result.nodes.concat(spaces.nodes),
+    links: spaces.links
+  };
+  spreadX(result.nodes);
+
+  // console.log(spaces);
+  return result;
 };
+
+createIdMaker = id => () => id++;
 
 getNextNodes = function(mat, current, next) {
   if (typeof current === 'undefined') {
@@ -156,40 +21,43 @@ getNextNodes = function(mat, current, next) {
       nodes: []
     };
   } else {
-    console.log('getNextNodeCalled with current.id =', current.getId());
     var newNodeIds = getNextIds(mat[current.getId()]);
     var newNodes = createNodesFromLinks(current, newNodeIds);
     var next = resolveConflicts(next.concat(newNodes));
-    console.log('next now', next);
+    // var links = createLinks(current, newNodes, makeId);
     return mergeResults(
       current,
-      newNodeIds,
+      newNodes,
       getNextNodes(mat, head(next), tail(next))
     );
   }
 }
 
 var getNextIds = function(nodeLinks) {
-  console.log('getNextIds called with adj =', nodeLinks);
   return nodeLinks.reduce((links, adj, nodeId) => (
     adj ? links.concat(nodeId) : links
   ), []);
 };
 
-var createNodesFromLinks = function(parent, links) {
-  console.log('createNodesFromLinks called with parentId =', parent.getId(), 'links =',links);
+var createNodesFromLinks = function(parent, links, type) {
   return links.map((linkId, offset) => createNode(
     linkId,
-    parent.getX() + offset,
-    parent.getY() + 1
+    parent.getX() + offset.toString(),
+    parent.getY() + 1,
+    type
   ));
 };
 
-var createNode = function(id, x, y) {
+var createNode = function(id, x_, y, type) {
+  console.log(id, x_);
+  var x = x_;
   return {
     getX: () => x,
+    setX: (newX) => x = newX,
     getY: () => y,
-    getId: () => id
+    getId: () => id,
+    getType: () => type
+    // console.log("node: {id: " + id + " x: " + x + " y: " + y + "}");
   };
 };
 
@@ -200,13 +68,54 @@ var head = function(arr) {
 
 var tail = function(arr) {
   return arr.slice(1);
-}
+};
 
-var mergeResults = function(node, links, prevResults) {
+var fillSpaces = function(nodes, links, createId) {
+  // console.log('>', i);
+  // console.log(linkIds.length);
+  console.log(nodes);
+
+  return links.reduce((result, link) => {
+    // console.log(link);
+    var i;
+    var parent;
+    var childNode = getNodeById(nodes, link[1]);
+    var parentNode = getNodeById(nodes, link[0]);
+    var yDiff = childNode.getY() - parentNode.getY();
+    if (yDiff === 1) {
+      return {
+        nodes: result.nodes,
+        links: result.links.concat([[parentNode.getId(), childNode.getId()]])
+      };
+    } else {
+      console.log('woah!', yDiff);
+      for (i = 0; i < yDiff - 1; i++) {
+        nextNode = createNodesFromLinks(parentNode, [createId()], 'stub')[0];
+        result.links.push([parentNode.getId(), nextNode.getId()]);
+        result.nodes.push(nextNode);
+        parentNode = nextNode;
+      }
+      result.links.push([parentNode.getId(), childNode.getId()])
+      return result;
+    }
+  },{
+    nodes: [],
+    links: []
+  });
+  // return targetNodes.reduce((result, targetNode, i) => {
+
+};
+
+var getNodeById = (nodes, id) => nodes.filter(node => node.getId() === id)[0];
+
+var mergeResults = function(node, newNodes, prevResults) {
   return {
-    nodes: prevResults.nodes.concat([node.getId]),
-    links: prevResults.links.concat(links.map(linkId => [node.getId, linkId]))
+    nodes: prevResults.nodes.concat([node]),
+    links: prevResults.links.concat(
+      newNodes.map(newNode => [node.getId(), newNode.getId()])
+    )
   };
+  // result.links.concat([[node.getId(), targetNode.getId()]])
 };
 
 var resolveConflicts = function(nextNodes) {
@@ -219,7 +128,7 @@ var resolveConflicts = function(nextNodes) {
       if (lastNode.getId() === node.getId()) {
         var mergeNode = createNode(
           lastNode.getId(),
-          lastNode.getX() < node.getX() ? lastNode.getX() : node.getX(),
+          compareX(lastNode, node) ? lastNode.getX() : node.getX(),
           lastNode.getY() > node.getY() ? lastNode.getY() : node.getY()
         );
         return uniqueNodes.slice(0, uniqueNodes.length-1).concat([mergeNode]);
@@ -232,7 +141,39 @@ var resolveConflicts = function(nextNodes) {
   }, [])
 }
 
-console.log(getNodes([
-  [0, 1],
-  [0, 0]
-]));
+var spreadX = function(nodes) {
+  var nodesInY = nodes.reduce((nodesInY, node) => {
+    // console.log(node.getX());
+    typeof nodesInY[node.getY()] !== 'undefined' ?
+      nodesInY[node.getY()].push(node) :
+      nodesInY[node.getY()]= [node]
+    return nodesInY;
+  }, []);
+  nodesInY = nodesInY.map(level => level.sort(compareX));
+  nodesInY.forEach(level => level.forEach((node, x) => {
+    node.setX((x + 1) / (level.length + 1))
+    console.log('>>', node.getX());
+  }));
+},
+
+compareX = function(x1, x2) {
+  var x1Arr = x1.getX().split('');
+  var x2Arr = x2.getX().split('');
+  var i = 0;
+  while (i < x1Arr.length) {
+    if (x1Arr[i] === '0' && x2Arr[i] === '1') return true;
+    else if (x1Arr[i] === '1' && x2Arr[i] === '0') return false;
+    i += 1;
+  }
+}
+
+getTestFlow = () => getNodes([
+  [0, 1, 1, 0, 0, 0, 0, 0], // 1 -> 2, 3
+  [0, 0, 0, 1, 0, 0, 0, 0], // 2 -> 4
+  [0, 0, 0, 0, 1, 1, 0, 0], // 3 -> 5, 6
+  [0, 0, 0, 0, 0, 0, 0, 1], // 4 -> 8
+  [0, 0, 0, 0, 0, 0, 1, 0], // 5 -> 7
+  [0, 0, 0, 0, 0, 0, 1, 0], // 6 -> 7
+  [0, 0, 0, 0, 0, 0, 0, 1], //
+  [0, 0, 0, 0, 0, 0, 0, 0]
+]);
