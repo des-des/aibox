441e43816fc4a9aede5c2e41ad77c48a6c95fb48
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-04-17 21:46:46 +0100

    move box drawing into component

---
+++ client/components/box.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react'
+import d3 from 'd3'
+
+export default class box extends Component {
+  render() {
+    return <div id = 'aibox'> </div>
+  }
+  componentDidMount() {
+    draw(this.props.graph);
+  }
+}
+
+const width = 300;
+
+// var getTestFlow = function() {
+//   state.pushNode(state.getOpenNodes());
+//   state.pushSplit(state.getOpenNodes());
+//   state.pushNode(state.getOpenNodes()[0]);
+//   state.pushNode(state.getOpenNodes()[0]);
+//   state.pushMerge(state.getOpenNodes());
+//
+//   console.log(executeFlow(-1, state.get(), [
+//     (x) => x*2,
+//     (x) => x < 0,
+//     (x) => x/2,
+//     (x) => x*2,
+//     (x) => x*2,
+//     (x) => x*2,
+//     (x) => x*2
+//   ]));
+//
+//   return state;
+// }
+
+
+// var drawFlow = function(graph) {
+  // var svgWrapper = getSvgWrapper();
+  // var flow = createFlowBuilder(function(flowGraph) {
+    // draw(graph, width, svgWrapper);
+  // });
+  // flow.initTestState();
+  // var testFlow = mat2Graph(flow.get());
+  // var openNodes = flow.getOpenNodes();
+  // console.log(testFlow.nodes[6]);
+  // console.log('testflow2!');
+  // console.log();
+// }
+
+const transform = {
+  x: x => x*200,
+  y: y => y*50 + 10
+}
+
+const getSvgWrapper = function() {
+  return document.getElementsByClassName('aibox')[0]
+}
+
+const getNodesAndLinksFromNodeLevel = (nodeLevel, nodeLevelDepth) => {
+  const nodeLevelWidth = nodeLevel.length;
+  return nodeLevel.reduce((levelRenderData, node, nodeNumber) => {
+    levelRenderData.nodes.push({
+      x: ((nodeNumber+1)/(nodeLevelWidth+1))*width,
+      y: (nodeLevelDepth)*100 + 10,
+      id: node.id,
+      data: node
+    })
+    if (node && node.next) {
+      node.next.forEach(function(link) {
+        levelRenderData.links.push([node.id, link])
+      })
+    }
+    return levelRenderData;
+  }, {
+    nodes: [],
+    links: []
+  })
+}
+
+const createRenderData = logicData => {
+  return logicData.reduce((renderData, nodeLevel, nodeLevelDepth) => {
+    const levelRenderData =
+      getNodesAndLinksFromNodeLevel(nodeLevel, nodeLevelDepth)
+
+    renderData.nodes = renderData.nodes.concat(levelRenderData.nodes)
+    renderData.links = renderData.links.concat(levelRenderData.links)
+    return renderData
+  }, {
+    nodes: [],
+    links: []
+  })
+}
+
+const draw = (renderData, width) => {
+    const svgContainer = drawContainer(width);
+    // var renderData = createRenderData(logicData);
+    drawLinks(svgContainer, renderData);
+    drawNodes(svgContainer, renderData.nodes);
+    drawLabels(svgContainer, renderData.nodes);
+}
+
+const drawContainer = width => {
+  return d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", 1200)
+}
+
+const drawLabels = (svgContainer, nodeData) => {
+  svgContainer.selectAll('text')
+    .data(nodeData)
+    .enter()
+    .append('text')
+    .attr("x", function(d) {
+      return transform.x(d.getX()) + 10;
+    })
+    .attr("y", function(d) { return transform.y(d.getY()); })
+    .text( function (d) { return d.getType() !== 'stub' ? d.getId() : ''; })
+    .attr("font-family", "Courier")
+    .attr("font-size", "12px")
+    .attr("fill", "white")
+}
+
+const drawLinks = (svgContainer, renderData) => {
+  const nodeData = renderData.nodes
+  const diagonal = d3.svg.diagonal()
+    .source(d => ({
+      "x" : transform.x(getNode(d[0], nodeData).getX()),
+      "y" : transform.y(getNode(d[0], nodeData).getY())
+    })).target(d => ({
+      "x" : transform.x(getNode(d[1], nodeData).getX()),
+      "y" : transform.y(getNode(d[1], nodeData).getY())
+    })).projection(d => ([d.x, d.y]))
+
+  svgContainer.selectAll('line')
+    .data(renderData.links)
+    .enter()
+    .append('path')
+    .attr("stroke-width", 2)
+    .attr("stroke", "white")
+    .attr('d', diagonal)
+}
+
+const drawNodes = (svgContainer, nodeData) => {
+  svgContainer.selectAll('circle')
+    .data(nodeData)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('cx', d => transform.x(d.getX()))
+    .attr('cy', d => transform.y(d.getY()))
+    .attr("fill", d => d.getType() !== "stub" ? "white" : "none")
+}
+
+const getNode = (id, nodeData) =>
+  nodeData.filter(node =>
+    node.getId() === id
+  )[0]
--- client/containers/flow_builder.js
+++ client/containers/flow_builder.js
@@ -2,19 +2,15 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import { adjMat2Graph } from './flow_builder/render_matrix.js'
+import Box from '../components/box.js'
 
-const mockAdj = [[1, 2], [], []];
-
-console.log(adjMat2Graph(mockAdj));
+const mockAdj = [[1, 2], [3], [4], [4], []];
+const mockGraph = adjMat2Graph(mockAdj);
 
 const flowBuilder = () =>
   <div>
-    adjacency matrix
-    <h3> {JSON.stringify(mockAdj)} </h3>
-    maps to graph
-    <h3> {JSON.stringify(adjMat2Graph(mockAdj))} </h3>
-    vvv test link vvv <br />
-    >>> <Link to="/public/sim"> sim </Link>
+    adjacency matrix <h3> {JSON.stringify(mockAdj)} </h3> yields graph:
+    <Box {...{graph: mockGraph}} />
   </div>
 
 flowBuilder.path = 'build'
