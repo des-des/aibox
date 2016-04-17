86e6659e4173a4e3dc3ea2dfe534e3252badc871
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-04-17 23:11:06 +0100

    immutablejs

--- client/create_store.js
+++ client/create_store.js
@@ -1,21 +1,24 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { combineReducers } from 'redux-immutable';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
+import Immutable from 'immutable'
 
 import reducers from './reducers/'
 
 const store = createStore(
   combineReducers({
     ...reducers,
-    routing: routerReducer
   }),
-  {},
+  Immutable.Map({}),
   applyMiddleware(...middleware)
 )
 
-export const history = syncHistoryWithStore(browserHistory, store)
+export const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: state => state.get('routing').toJS()
+})
 
 const middleware = [createLogger()]
 
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,1 +1,7 @@
-export flowBuilder from './flow_builder.js'
+import flowBuilder from './flow_builder.js'
+import routing from './routing.js'
+
+export default {
+  flow: flowBuilder,
+  routing
+}
