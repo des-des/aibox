094e24b4ca2d3a2271d33c7cb1d66be55a00e4e3
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-04-17 21:15:40 +0100

    integrate graph build with app

--- client/containers/flow_builder.js
+++ client/containers/flow_builder.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import { adjMat2Graph } from './flow_builder/render_matrix.js'
+
+const mockAdj = [[1, 2], [], []];
+
+console.log(adjMat2Graph(mockAdj));
 
 const flowBuilder = () =>
   <div>
-    <Link to="/public/sim"> sim </Link>
-    this is the flow builder!
+    adjacency matrix
+    <h3> {JSON.stringify(mockAdj)} </h3>
+    maps to graph
+    <h3> {JSON.stringify(adjMat2Graph(mockAdj))} </h3>
+    vvv test link vvv <br />
+    >>> <Link to="/public/sim"> sim </Link>
   </div>
 
 flowBuilder.path = 'build'
--- client/containers/flow_builder/render_matrix.js
+++ client/containers/flow_builder/render_matrix.js
@@ -1,164 +1,156 @@
-const mat2Graph = (() => {
+const createIdMaker = id => () => id++
 
-  const createIdMaker = id => () => id++;
-
-  const getNextNodes = function(mat, current, next) {
-    if (typeof current === 'undefined') {
-      return {
-        links: [],
-        nodes: []
-      };
-    } else {
-      var newNodeIds = mat[current.getId()];
-      var newNodes = createNodesFromLinks(current, newNodeIds);
-      var next = resolveConflicts(next.concat(newNodes));
-      return mergeResults(
-        current,
-        newNodes,
-        getNextNodes(mat, head(next), tail(next))
-      );
-    }
-  };
-
-  const createNodesFromLinks = function(parent, links, type) {
-    return links.map((linkId, offset) => createNode(
-      linkId,
-      parent.getX() + offset.toString(),
-      parent.getY() + 1,
-      type
-    ));
-  };
-
-  const createNode = function(id, x_, y, type) {
-    var x = x_;
+const getNextNodes = function(mat, current, next) {
+  if (typeof current === 'undefined') {
     return {
-      getX: () => x,
-      setX: newX => x = newX,
-      getY: () => y,
-      getId: () => id,
-      getType: () => type
-    };
-  };
+      links: [],
+      nodes: []
+    }
+  } else {
+    const newNodeIds = mat[current.getId()]
+    const newNodes = createNodesFromLinks(current, newNodeIds)
+    next = resolveConflicts(next.concat(newNodes))
+    return mergeResults(
+      current,
+      newNodes,
+      getNextNodes(mat, head(next), tail(next))
+    )
+  }
+}
 
+const createNodesFromLinks = function(parent, links, type) {
+  return links.map((linkId, offset) => createNode(
+    linkId,
+    parent.getX() + offset.toString(),
+    parent.getY() + 1,
+    type
+  ))
+}
 
-  const head = function(arr) {
-    return arr[0];
-  };
+const createNode = function(id, x_, y, type) {
+  let x = x_
+  return {
+    getX: () => x,
+    setX: newX => x = newX,
+    getY: () => y,
+    getId: () => id,
+    getType: () => type
+  }
+}
 
-  const tail = function(arr) {
-    return arr.slice(1);
-  };
+const head = function(arr) {
+  return arr[0]
+}
 
-  const fillSpaces = function(nodes, links, createId) {
-    return links.reduce((result, link) => {
-      let i;
-      let parent;
-      let childNode = getNodeById(nodes, link[1]);
-      let parentNode = getNodeById(nodes, link[0]);
-      let yDiff = childNode.getY() - parentNode.getY();
-      if (yDiff === 1) {
-        return {
-          nodes: result.nodes,
-          links: result.links.concat([[parentNode.getId(), childNode.getId()]])
-        };
-      } else {
-        for (i = 0; i < yDiff - 1; i++) {
-          let nextNode = createNodesFromLinks(parentNode, [createId()], 'stub')[0];
-          result.links.push([parentNode.getId(), nextNode.getId()]);
-          result.nodes.push(nextNode);
-          parentNode = nextNode;
-        }
-        result.links.push([parentNode.getId(), childNode.getId()])
-        return result;
+const tail = function(arr) {
+  return arr.slice(1)
+}
+
+const fillSpaces = function(nodes, links, createId) {
+  return links.reduce((result, link) => {
+    let i
+    let parent
+    let parentNode = getNodeById(nodes, link[0])
+
+    const childNode = getNodeById(nodes, link[1])
+    const yDiff = childNode.getY() - parentNode.getY()
+
+    if (yDiff === 1) {
+      return {
+        nodes: result.nodes,
+        links: result.links.concat([[parentNode.getId(), childNode.getId()]])
       }
-    },{
+    } else {
+      for (i = 0; i < yDiff - 1; i++) {
+        const nextNode =
+          createNodesFromLinks(parentNode, [createId()], 'stub')[0]
+        result.links.push([parentNode.getId(), nextNode.getId()])
+        result.nodes.push(nextNode)
+        parentNode = nextNode
+      }
+      result.links.push([parentNode.getId(), childNode.getId()])
+      return result
+    }
+  },{
+    nodes: [],
+    links: []
+  })
+}
+
+const getNodeById = (nodes, id) => nodes.filter(node => node.getId() === id)[0]
+
+const mergeResults = function(node, newNodes, prevResults) {
+  return {
+    nodes: prevResults.nodes.concat([node]),
+    links: prevResults.links.concat(
+      newNodes.map(newNode => [node.getId(), newNode.getId()])
+    )
+  }
+}
+
+const resolveConflicts = function(nextNodes) {
+  const sortedNodes = nextNodes.sort((lNode, rNode) => (
+    lNode.getId() > rNode.getId()
+  ))
+  return sortedNodes.reduce((uniqueNodes, node) => {
+    if (uniqueNodes.length > 0) {
+      var lastNode = uniqueNodes[uniqueNodes.length - 1]
+      if (lastNode.getId() === node.getId()) {
+        var mergeNode = createNode(
+          lastNode.getId(),
+          compareX(lastNode, node) ? lastNode.getX() : node.getX(),
+          lastNode.getY() > node.getY() ? lastNode.getY() : node.getY()
+        )
+        return uniqueNodes.slice(0, uniqueNodes.length-1).concat([mergeNode])
+      } else {
+        return uniqueNodes.concat([node])
+      }
+    } else {
+      return uniqueNodes.concat([node])
+    }
+  }, [])
+}
+
+const spreadX = function(nodes) {
+  let nodesInY = nodes.reduce((nodesInY, node) => {
+    typeof nodesInY[node.getY()] !== 'undefined' ?
+      nodesInY[node.getY()].push(node) :
+      nodesInY[node.getY()]= [node]
+    return nodesInY;
+  }, [])
+  nodesInY = nodesInY.map(level => level.sort(compareX));
+  nodesInY.forEach(level => level.forEach((node, x) => {
+    node.setX((x + 1) / (level.length + 1))
+  }))
+}
+
+const compareX = function(x1, x2) {
+  const x1Arr = x1.getX().split('')
+  const x2Arr = x2.getX().split('')
+  let i = 0
+  while (i < x1Arr.length) {
+    if (x1Arr[i] === '0' && x2Arr[i] === '1') return true
+    else if (x1Arr[i] === '1' && x2Arr[i] === '0') return false
+    i += 1
+  }
+}
+
+export const adjMat2Graph = adjacency => {
+  if (adjacency.length === 1 && adjacency[0].length === 0) {
+    return {
       nodes: [],
       links: []
-    });
-  };
-
-  const getNodeById = (nodes, id) => nodes.filter(node => node.getId() === id)[0];
-
-  const mergeResults = function(node, newNodes, prevResults) {
-    return {
-      nodes: prevResults.nodes.concat([node]),
-      links: prevResults.links.concat(
-        newNodes.map(newNode => [node.getId(), newNode.getId()])
-      )
-    };
-    // result.links.concat([[node.getId(), targetNode.getId()]])
-  };
-
-  const resolveConflicts = function(nextNodes) {
-    var sortedNodes = nextNodes.sort((lNode, rNode) => (
-      lNode.getId() > rNode.getId()
-    ));
-    return sortedNodes.reduce((uniqueNodes, node) => {
-      if (uniqueNodes.length > 0) {
-        var lastNode = uniqueNodes[uniqueNodes.length - 1];
-        if (lastNode.getId() === node.getId()) {
-          var mergeNode = createNode(
-            lastNode.getId(),
-            compareX(lastNode, node) ? lastNode.getX() : node.getX(),
-            lastNode.getY() > node.getY() ? lastNode.getY() : node.getY()
-          );
-          return uniqueNodes.slice(0, uniqueNodes.length-1).concat([mergeNode]);
-        } else {
-          return uniqueNodes.concat([node]);
-        }
-      } else {
-        return uniqueNodes.concat([node]);
-      }
-    }, [])
+    }
   }
 
-  const spreadX = function(nodes) {
-    let nodesInY = nodes.reduce((nodesInY, node) => {
-      typeof nodesInY[node.getY()] !== 'undefined' ?
-        nodesInY[node.getY()].push(node) :
-        nodesInY[node.getY()]= [node]
-      return nodesInY;
-    }, []);
-    nodesInY = nodesInY.map(level => level.sort(compareX));
-    nodesInY.forEach(level => level.forEach((node, x) => {
-      node.setX((x + 1) / (level.length + 1))
-    }));
-  };
+  const makeId = createIdMaker(adjacency.length)
 
-  const compareX = function(x1, x2) {
-    var x1Arr = x1.getX().split('');
-    var x2Arr = x2.getX().split('');
-    var i = 0;
-    while (i < x1Arr.length) {
-      if (x1Arr[i] === '0' && x2Arr[i] === '1') return true;
-      else if (x1Arr[i] === '1' && x2Arr[i] === '0') return false;
-      i += 1;
-    }
-  };
-
-
-
-  return (adjacency) => {
-    if (adjacency.length === 1 && adjacency[0].length === 0) {
-      return {
-        nodes: [],
-        links: []
-      }
-    }
-
-    const makeId = createIdMaker(adjacency.length);
-
-    let graph = getNextNodes(adjacency, createNode(0, '', 0), []);
-    const emptyNodes = fillSpaces(graph.nodes, graph.links, makeId);
-    graph = {
-      nodes: graph.nodes.concat(emptyNodes.nodes),
-      links: emptyNodes.links
-    };
-    spreadX(graph.nodes); // not pure...
-    return graph;
+  let graph = getNextNodes(adjacency, createNode(0, '', 0), [])
+  const emptyNodes = fillSpaces(graph.nodes, graph.links, makeId)
+  graph = {
+    nodes: graph.nodes.concat(emptyNodes.nodes),
+    links: emptyNodes.links
   }
-})();
-
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = mat2Graph;
+  spreadX(graph.nodes) // not pure...
+  return graph
 }
